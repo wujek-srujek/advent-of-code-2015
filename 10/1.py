5754628af3e221a349d0e3a3924ae1725a5242a0
Wujek Srujek <wujek.srujek@gmail.com>
2019-01-02 22:04:53 +0100

    Day 10

---
+++ 10/1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+
+def run(line, iterations):
+    def look_and_say(line):
+        res = []
+        length = len(line)
+        i = 0
+        while i < length:
+            c = line[i]
+            count = 0
+            while i < length and line[i] == c:
+                count += 1
+                i += 1
+            res.extend((str(count), c))
+        return ''.join(res)
+
+    for i in range(iterations):
+        line = look_and_say(line)
+    return len(line)
+
+
+with open('input.txt') as lines:
+    for line in lines:
+        print(run(line.strip(), 40))
